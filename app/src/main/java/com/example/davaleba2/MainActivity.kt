32080fc80e5fed274ed0c7a977464d25dc1fb1a0
Alexander Kot <aleko.kotliarovi@gmail.com>
2021-11-07 01:24:02 +0400

    Update

--- app/src/main/java/com/example/davaleba2/MainActivity.kt
+++ app/src/main/java/com/example/davaleba2/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.davaleba2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity()
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var click = findViewById<TextView>(R.id.button)
+        click.setOnClickListener {
+            Toast.makeText(this,"2500$",Toast.LENGTH_LONG).show()
+        }
     }
 }
