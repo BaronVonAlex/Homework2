f473da117f19d2522a5374868d2de223db9c1f7e
Alexander Kot <aleko.kotliarovi@gmail.com>
2021-11-07 01:34:01 +0400

    Visual Update and Button is working (probably) can't test

--- app/src/main/java/com/example/davaleba2/MainActivity.kt
+++ app/src/main/java/com/example/davaleba2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.davaleba2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var click = findViewById<TextView>(R.id.button)
+        val click = findViewById<Button>(R.id.button)
         click.setOnClickListener {
             Toast.makeText(this,"2500$",Toast.LENGTH_LONG).show()
         }
